328955db46f79da6be4c6d00c5f264d26f07ccf0
wajeeha09 <wajeeha.ahmed09@gmail.com>
2025-09-09 19:02:30 +0500

    Adds SwaggerConfig

---
+++ src/main/java/com/test/tms/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.test.tms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("JWT Authentication API")
+                        .version("1.0")
+                        .description("Spring Boot JWT Authentication API with user registration and login")
+                        )
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .name("Bearer Authentication")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
